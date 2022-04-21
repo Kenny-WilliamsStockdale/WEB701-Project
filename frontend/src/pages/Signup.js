7062f96e94c6b0124d98688b022d5069c220969e
Kenny <kenny-williamsStockd@live.nmit.ac.nz>
2022-04-21 12:43:24 +1200

    added signup styling

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -9,31 +9,33 @@ import './Signup.css'
 /* -------------------------------------------------------------------------- */
 const Signup = () => {
   return (
-    <form>
-      <h1 className="h3 mb-3 fw-normal">Signup</h1>
-      <div className="form-floating">
-        <input type="first-name" className="form-control" id="floatingInput" placeholder="FirstName" />
-        <label htmlFor="floatingInput">First Name</label>
-      </div>
-      <div className="form-floating">
-        <input type="last-name" className="form-control" id="floatingInput" placeholder="LastName" />
-        <label htmlFor="floatingInput">Last Name</label>
-      </div>
-      <div className="form-floating">
-        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-        <label htmlFor="floatingInput">Email address</label>
-      </div>
-      <div className="form-floating">
-        <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-        <label htmlFor="floatingPassword">Password</label>
-      </div>
-      <div className="form-floating">
-        <input type="confirm-password" className="form-control" id="floatingPassword" placeholder="ConfirmPassword" />
-        <label htmlFor="floatingPassword">Confirm Password</label>
-      </div>
-      <button className="w-100 btn btn-lg btn-primary" type="submit">Register</button>
-      <Link className="signup-link"to="/Login">Already have an account?</Link>
-    </form>
+    <main className="form-signin">
+      <form>
+        <h1 className="h3 mb-3 fw-normal" id="signup-title">Signup</h1>
+        <div className="form-floating">
+          <input type="first-name" className="form-control" id="floatingInput" placeholder="FirstName" />
+          <label htmlFor="floatingInput">First Name</label>
+        </div>
+        <div className="form-floating">
+          <input type="last-name" className="form-control" id="floatingInput" placeholder="LastName" />
+          <label htmlFor="floatingInput">Last Name</label>
+        </div>
+        <div className="form-floating">
+          <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+          <label htmlFor="floatingInput">Email address</label>
+        </div>
+        <div className="form-floating">
+          <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+          <label htmlFor="floatingPassword">Password</label>
+        </div>
+        <div className="form-floating">
+          <input type="confirm-password" className="form-control" id="floatingPassword-signup" placeholder="ConfirmPassword" />
+          <label htmlFor="floatingPassword">Confirm Password</label>
+        </div>
+        <button className="w-100 btn btn-lg btn-primary" id="submit-button-signup" type="submit">Register</button>
+        <Link className="signup-link" to="/Login">Already have an account?</Link>
+      </form>
+    </main>
   )
 }
 

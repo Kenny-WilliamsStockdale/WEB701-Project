c26985d4b0595fad92dd70d5de23acd6ac4fcf25
Kenny <kenny-williamsStockd@live.nmit.ac.nz>
2022-06-14 10:53:05 +1200

    added top landing page area testing

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -2,6 +2,7 @@
 /*                               Import Section                               */
 /* -------------------------------------------------------------------------- */
 import React from 'react'
+import landingPic from '../Imgs/landing-picture.jpg'
 import './Landing.css'
 /* -------------------------------------------------------------------------- */
 /*                               Layout Section                               */
@@ -9,9 +10,17 @@ import './Landing.css'
 
 const Landing = () => {
   return (
-    <div>
-      <h1 className="intro-text">Welcome to Stuff and Things Clothing Charity</h1>
+    <><div className="landing-top">
+      <div className="intro-text-container">
+        <h1 className="intro-text">Welcome to Stuff and Things Clothing Charity</h1>
+      </div>
+      <div className="landing-pic-container">
+        <img className="landing-pic" src={landingPic} />
+      </div>
     </div>
+      <div className="landing-mid">
+        <h1 className="intro-text">We are a non-profit organization that provides clothing to the homeless people in the United States.</h1>
+      </div></>
   )
 }
 

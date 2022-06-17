5a5483ede3cd54fc0fee0129eda45b6e522fb896
Kenny <kenny-williamsStockd@live.nmit.ac.nz>
2022-06-17 23:00:52 +1200

    added guard clause for shop now button for members and beneiciaries

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -5,7 +5,7 @@ import React from 'react'
 import { FaGgCircle } from 'react-icons/fa';
 import { FaGratipay } from 'react-icons/fa';
 import { FaMegaport } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import landingPic from '../Imgs/landing-picture.jpg'
 import './Landing.css'
 /* -------------------------------------------------------------------------- */
@@ -13,13 +13,27 @@ import './Landing.css'
 /* -------------------------------------------------------------------------- */
 
 const Landing = () => {
+  const navigate = useNavigate()
+  const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+  //guarded route to check if user is logged in if not go to login page
+  const guardedRoute = () => {
+    if (localStorage.getItem('userInfo') === null) {
+      navigate('/Login')
+    }
+    if (userInfo.data.isMember === true) {
+      navigate('/addProduct')
+    }
+    else {
+      navigate('/Product')
+    }
+  }
+
+
   return (
     <><div className="landing-top">
       <div className="intro-text-container">
         <h1 className="intro-text-top">Welcome to <br /> Stuff and Things Clothing Charity</h1>
-        <Link to="/Product">
-          <button className="btn btn-primary btn-lg" id="landing-shopNow-btn">Shop Now</button>
-        </Link>
+        <button className="btn btn-primary btn-lg" id="landing-shopNow-btn" onClick={guardedRoute}>Shop Now</button>
       </div>
       <div className="landing-pic-container">
         <img className="landing-pic" src={landingPic} />
